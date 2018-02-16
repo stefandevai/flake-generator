fc7d31a36f37a3c8fd1302a76f749dbfc3a5d8ee
Stefan Devai <stedevai@gmail.com>
2018-02-16 16:43:16 +0200

    Generating a Sierpinski Pentagon

--- nflakes.py
+++ nflakes.py
@@ -13,6 +13,8 @@ SCREEN_COLOR = (52, 9, 38)
 FLAKE_COLOR = (255, 195, 31)
 CONSTANT = 1.0/(1.0 + ((1.0 + math.sqrt(5))/2.0))
 
+ROTATION = 90
+
 class Polygon:
     def __init__(self, nv, rd, ct):
         self.num_vertices = nv
@@ -20,15 +22,21 @@ class Polygon:
         self.center = ct
         self.angle_base = 360/self.num_vertices
 
-    def draw(self, win):
+    def draw(self, win, inv):
         coords = []
         for i in range(0, self.num_vertices):
-            cx = round(self.center[0] + self.radius*math.cos(math.radians(self.angle_base*i + 90)))
-            cy = round(self.center[1] - self.radius*math.sin(math.radians(self.angle_base*i + 90)))
+
+            if inv == True:
+                cx = self.center[0] + self.radius*math.cos(math.radians(self.angle_base*i + ROTATION))
+                cy = self.center[1] + self.radius*math.sin(math.radians(self.angle_base*i + ROTATION))
+            else:
+                cx = self.center[0] + self.radius*math.cos(math.radians(self.angle_base*i + ROTATION))
+                cy = self.center[1] - self.radius*math.sin(math.radians(self.angle_base*i + ROTATION))
+
             coords.append((cx, cy))
 
         pg.gfxdraw.aapolygon(win, coords, FLAKE_COLOR)
-        # pg.draw.polygon(win, FLAKE_COLOR, coords, 0)
+        pg.gfxdraw.filled_polygon(win, coords, FLAKE_COLOR)
 
 class Flake:
     def __init__(self, nv, rd, ct):
@@ -37,22 +45,29 @@ class Flake:
         self.center = ct
         self.angle_base = 360/self.num_vertices
 
-    def __draw__(self, steps, rd, ct):
+    def __draw__(self, steps, rd, ct, inv):
         if steps <= 0:
             pass
         elif steps == 1:
             poly = Polygon(self.num_vertices, rd, ct)
-            poly.draw(self.screen_ref)
+            poly.draw(self.screen_ref, inv)
         else:
+            self.__draw__(steps-1, rd*CONSTANT, ct, not inv)
             for i in range(0, self.num_vertices):
-                cx = round(ct[0] + rd*math.cos(math.radians((self.angle_base)*i + 90)) - rd*CONSTANT*math.cos(math.radians((self.angle_base)*i + 90)))
-                cy = round(ct[1] - rd*math.sin(math.radians((self.angle_base)*i + 90)) + rd*CONSTANT*math.sin(math.radians((self.angle_base)*i + 90)))
-                self.__draw__(steps-1, rd*CONSTANT, (cx, cy))
+
+                if inv == True:
+                    cx = ct[0] + rd*math.cos(math.radians((self.angle_base)*i + ROTATION)) - rd*CONSTANT*math.cos(math.radians((self.angle_base)*i + ROTATION))
+                    cy = ct[1] + rd*math.sin(math.radians((self.angle_base)*i + ROTATION)) - rd*CONSTANT*math.sin(math.radians((self.angle_base)*i + ROTATION))
+                else:
+                    cx = ct[0] + rd*math.cos(math.radians((self.angle_base)*i + ROTATION)) - rd*CONSTANT*math.cos(math.radians((self.angle_base)*i + ROTATION))
+                    cy = ct[1] + rd*math.sin(math.radians((self.angle_base)*i - ROTATION)) + rd*CONSTANT*math.sin(math.radians((self.angle_base)*i + ROTATION))
+
+                self.__draw__(steps-1, rd*CONSTANT, (cx, cy), inv)
 
     def draw(self, st, win):
         self.steps = st
         self.screen_ref = win
-        self.__draw__(self.steps, self.radius, self.center)
+        self.__draw__(self.steps, self.radius, self.center, False)
 
 def main():
     pg.init()
@@ -61,8 +76,8 @@ def main():
     screen.fill(SCREEN_COLOR)
 
     center = (SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
-    nflake = Flake(7, 300, center)
-    nflake.draw(4, screen)
+    nflake = Flake(5, 300, center)
+    nflake.draw(5, screen)
 
     running = True
     while running == True:
