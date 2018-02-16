745e4d16bab2ec0781cc7a097a9be3a3ac8415e6
Stefan Devai <stedevai@gmail.com>
2018-02-16 11:53:16 +0200

    Recursive function refactored

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # N-flakes
 https://en.wikipedia.org/wiki/N-flake
+
+# TODO:
+- Keep variables as floats so distortion gets less worse in higher steps
--- nflakes.py
+++ nflakes.py
@@ -9,6 +9,7 @@ SCREEN_HEIGHT = 700
 SCREEN_COLOR = (100, 100, 100)
 
 FLAKE_COLOR = (255, 150, 70)
+CONSTANT = 1.0/(1.0 + ((1.0 + math.sqrt(5))/2.0))
 
 class Polygon:
     def __init__(self, nv, rd, ct):
@@ -26,27 +27,39 @@ class Polygon:
 
         pg.draw.polygon(win, FLAKE_COLOR, coords, 0)
 
-gr = (1.0 + math.sqrt(5))/2.0
-CONSTANT = 1.0/(1.0 + gr)
-def draw_nflake(steps, nv, rd, ct, win):
-    if steps <= 0:
-        pass
-    elif steps == 1:
-        poly = Polygon(nv, rd, ct)
-        poly.draw(win)
-        draw_nflake(steps-1, nv, rd, ct, win)
-    else:
-        poly = Polygon(nv, rd, ct)
-        for i in range(0, nv):
-            cx = round(ct[0] + rd*math.cos(math.radians((360/nv)*i + 90)) - rd*CONSTANT*math.cos(math.radians((360/nv)*i + 90)))
-            cy = round(ct[1] - rd*math.sin(math.radians((360/nv)*i + 90)) + rd*CONSTANT*math.sin(math.radians((360/nv)*i + 90)))
-            draw_nflake(steps-1, nv, rd*CONSTANT, (cx, cy), win)
+class Flake:
+    def __init__(self, nv, rd, ct):
+        self.num_vertices = nv
+        self.radius = rd
+        self.center = ct
+        self.angle_base = 360/self.num_vertices
+
+    def __draw__(self, steps, rd, ct):
+        if steps <= 0:
+            pass
+        elif steps == 1:
+            poly = Polygon(self.num_vertices, rd, ct)
+            poly.draw(self.screen_ref)
+        else:
+            for i in range(0, self.num_vertices):
+                cx = round(ct[0] + rd*math.cos(math.radians((self.angle_base)*i + 90)) - rd*CONSTANT*math.cos(math.radians((self.angle_base)*i + 90)))
+                cy = round(ct[1] - rd*math.sin(math.radians((self.angle_base)*i + 90)) + rd*CONSTANT*math.sin(math.radians((self.angle_base)*i + 90)))
+                self.__draw__(steps-1, rd*CONSTANT, (cx, cy))
+
+    def draw(self, st, win):
+        self.steps = st
+        self.screen_ref = win
+        self.__draw__(self.steps, self.radius, self.center)
 
 def main():
     pg.init()
     pg.display.set_caption('n-flakes')
     screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     screen.fill(SCREEN_COLOR)
+
+    center = (SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    nflake = Flake(5, 300, center)
+    nflake.draw(4, screen)
 
     running = True
     while running == True:
@@ -57,7 +70,6 @@ def main():
             elif e.type == QUIT:
                 running = False
 
-        draw_nflake(4, 5, 300, (SCREEN_WIDTH/2, SCREEN_HEIGHT/2), screen)
         pg.display.flip()
 
     pg.quit()
