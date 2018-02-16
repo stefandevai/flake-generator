cd13b008f8428ead171ee73b997b693e3dc6fb2a
Stefan Devai <stedevai@gmail.com>
2018-02-16 20:33:01 +0200

    Added a small demo

--- Flake.py
+++ Flake.py
@@ -3,19 +3,24 @@ import pygame as pg
 
 from Polygon import *
 
-ROTATION = 90
 
 class Flake:
+    ROTATION = 90
+    COLORS = [(252, 224, 146), (252, 183, 150), (179, 135, 120), (148, 116, 107), (91, 78, 83), (57, 56, 69), (47, 50, 65)]
+
     def __init__(self, nv, rd, ct):
         self.num_vertices = nv
         self.radius = rd
         self.center = ct
         self.angle_base = 360/self.num_vertices
 
+        self.colorful = False
+        self.outlined = False
+
     def draw(self, st, win):
         self.steps = st
         self.screen_ref = win
-        self.scale_factor = self.__get_scale_factor()
+        self.__get_scale_factor()
 
         # If there are too few flakes to make a polygon, or if it's a pseudo circle
         if self.num_vertices < 3 or self.num_vertices > 20:
@@ -37,19 +42,31 @@ class Flake:
         aux = 0
         for i in range(0, self.num_vertices//4):
             aux += math.cos(2*math.pi*(i+1) / self.num_vertices)
-        return 1 / (2*(1 + aux))
+        self.scale_factor = 1 / (2*(1 + aux))
 
     def __draw_hexaflake(self, steps, rd, ct):
         if steps <= 0:
             pass
         elif steps == 1:
             poly = Polygon(self.num_vertices, rd, ct)
+            poly.color = self.COLORS[steps-1]
+            if self.outlined == True:
+                poly.fill = False
+
             poly.draw(self.screen_ref, 1)
         else:
+            if self.colorful == True:
+                poly = Polygon(self.num_vertices, rd, ct)
+                poly.color = self.COLORS[steps-1]
+                if self.outlined == True:
+                    poly.fill = False
+
+                poly.draw(self.screen_ref, 1)
+
             self.__draw_hexaflake(steps-1, rd*self.scale_factor, ct)
             for i in range(0, self.num_vertices):
-                cx = ct[0] + rd*math.cos(math.radians((self.angle_base)*i + ROTATION)) - rd*self.scale_factor*math.cos(math.radians((self.angle_base)*i + ROTATION))
-                cy = ct[1] + rd*math.sin(math.radians((self.angle_base)*i - ROTATION)) + rd*self.scale_factor*math.sin(math.radians((self.angle_base)*i + ROTATION))
+                cx = ct[0] + rd*math.cos(math.radians((self.angle_base)*i + self.ROTATION)) - rd*self.scale_factor*math.cos(math.radians((self.angle_base)*i + self.ROTATION))
+                cy = ct[1] + rd*math.sin(math.radians((self.angle_base)*i - self.ROTATION)) + rd*self.scale_factor*math.sin(math.radians((self.angle_base)*i + self.ROTATION))
                 self.__draw_hexaflake(steps-1, rd*self.scale_factor, (cx, cy))
 
     def __draw_pentaflake(self, steps, rd, ct, inv):
@@ -57,12 +74,24 @@ class Flake:
             pass
         elif steps == 1:
             poly = Polygon(self.num_vertices, rd, ct)
+            poly.color = self.COLORS[steps-1]
+            if self.outlined == True:
+                poly.fill = False
+
             poly.draw(self.screen_ref, inv)
         else:
+            if self.colorful == True:
+                poly = Polygon(self.num_vertices, rd, ct)
+                poly.color = self.COLORS[steps-1]
+                if self.outlined == True:
+                    poly.fill = False
+
+                poly.draw(self.screen_ref, inv)
+
             self.__draw_pentaflake(steps-1, rd*self.scale_factor, ct, -inv)
             for i in range(0, self.num_vertices):
-                cx = ct[0] + rd*math.cos(math.radians((self.angle_base)*i + ROTATION)) - rd*self.scale_factor*math.cos(math.radians((self.angle_base)*i + ROTATION))
-                cy = ct[1] + rd*math.sin(math.radians((self.angle_base)*i - inv*ROTATION)) + inv*rd*self.scale_factor*math.sin(math.radians((self.angle_base)*i + ROTATION))
+                cx = ct[0] + rd*math.cos(math.radians((self.angle_base)*i + self.ROTATION)) - rd*self.scale_factor*math.cos(math.radians((self.angle_base)*i + self.ROTATION))
+                cy = ct[1] + rd*math.sin(math.radians((self.angle_base)*i - inv*self.ROTATION)) + inv*rd*self.scale_factor*math.sin(math.radians((self.angle_base)*i + self.ROTATION))
                 self.__draw_pentaflake(steps-1, rd*self.scale_factor, (cx, cy), inv)
 
     def __draw_vicsek(self, steps, rd, ct):
@@ -70,12 +99,24 @@ class Flake:
             pass
         elif steps == 1:
             poly = Polygon(self.num_vertices, rd, ct)
+            poly.color = self.COLORS[steps-1]
+            if self.outlined == True:
+                poly.fill = False
+
             poly.draw(self.screen_ref, 1)
         else:
+            if self.colorful == True:
+                poly = Polygon(self.num_vertices, rd, ct)
+                poly.color = self.COLORS[steps-1]
+                if self.outlined == True:
+                    poly.fill = False
+
+                poly.draw(self.screen_ref, 1)
+
             self.__draw_vicsek(steps-1, rd*self.scale_factor, ct)
             for i in range(0, self.num_vertices):
-                cx = ct[0] + rd*math.cos(math.radians((self.angle_base)*i + ROTATION)) - rd*self.scale_factor*math.cos(math.radians((self.angle_base)*i + ROTATION))
-                cy = ct[1] + rd*math.sin(math.radians((self.angle_base)*i - ROTATION)) + rd*self.scale_factor*math.sin(math.radians((self.angle_base)*i + ROTATION))
+                cx = ct[0] + rd*math.cos(math.radians((self.angle_base)*i + self.ROTATION)) - rd*self.scale_factor*math.cos(math.radians((self.angle_base)*i + self.ROTATION))
+                cy = ct[1] + rd*math.sin(math.radians((self.angle_base)*i - self.ROTATION)) + rd*self.scale_factor*math.sin(math.radians((self.angle_base)*i + self.ROTATION))
                 self.__draw_vicsek(steps-1, rd*self.scale_factor, (cx, cy))
 
     def __draw_nflake(self, steps, rd, ct):
@@ -83,10 +124,22 @@ class Flake:
             pass
         elif steps == 1:
             poly = Polygon(self.num_vertices, rd, ct)
+            poly.color = self.COLORS[steps-1]
+            if self.outlined == True:
+                poly.fill = False
+
             poly.draw(self.screen_ref, 1)
         else:
+            if self.colorful == True:
+                poly = Polygon(self.num_vertices, rd, ct)
+                poly.color = self.COLORS[steps-1]
+                if self.outlined == True:
+                    poly.fill = False
+
+                poly.draw(self.screen_ref, 1)
+
             for i in range(0, self.num_vertices):
-                cx = ct[0] + rd*math.cos(math.radians((self.angle_base)*i + ROTATION)) - rd*self.scale_factor*math.cos(math.radians((self.angle_base)*i + ROTATION))
-                cy = ct[1] + rd*math.sin(math.radians((self.angle_base)*i - ROTATION)) + rd*self.scale_factor*math.sin(math.radians((self.angle_base)*i + ROTATION))
+                cx = ct[0] + rd*math.cos(math.radians((self.angle_base)*i + self.ROTATION)) - rd*self.scale_factor*math.cos(math.radians((self.angle_base)*i + self.ROTATION))
+                cy = ct[1] + rd*math.sin(math.radians((self.angle_base)*i - self.ROTATION)) + rd*self.scale_factor*math.sin(math.radians((self.angle_base)*i + self.ROTATION))
                 self.__draw_nflake(steps-1, rd*self.scale_factor, (cx, cy))
 
--- nflakes.py
+++ nflakes.py
@@ -6,22 +6,62 @@ from pygame.locals import *
 from Polygon import *
 from Flake import *
 
-SCREEN_WIDTH = 900
-SCREEN_HEIGHT = 700
-SCREEN_COLOR = (52, 9, 38)
+SCREEN_WIDTH = 760
+SCREEN_HEIGHT = 530
+SCREEN_COLOR = (31, 27, 27)
 
 def main():
-    vertices = int(input("Vertices: "))
-    iterations = int(input("Iterations: "))
+    # First ask user for input
+    # vertices = int(input("Vertices: "))
+    # iterations = int(input("Iterations: "))
 
     pg.init()
     pg.display.set_caption('n-flakes')
     screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     screen.fill(SCREEN_COLOR)
 
-    center = (SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
-    nflake = Flake(vertices, 300, center)
-    nflake.draw(iterations, screen)
+    # center = (SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    # nflake = Flake(vertices, 100, center)
+    # nflake.colorful = True
+    # nflake.outlined = True
+    # nflake.draw(iterations, screen)
+
+    its = 4
+    rad = 100
+    pos = (150, 150)
+    col = True
+    out = True
+
+    flake3 = Flake(3, rad, (pos[0], pos[1]))
+    flake3.colorful = col
+    flake3.outlined = out
+    flake3.draw(its+2, screen)
+
+    flake4 = Flake(4, rad, (pos[0] + rad*2 + 30, pos[1]))
+    flake4.colorful = col
+    flake4.outlined = out
+    flake4.draw(its+1, screen)
+
+    flake5 = Flake(5, rad, (pos[0] + rad*4 + 60, pos[1]))
+    flake5.colorful = col
+    flake5.outlined = out
+    flake5.draw(its, screen)
+
+    flake6 = Flake(6, rad, (pos[0], pos[1] + rad*2 + 30))
+    flake6.colorful = col
+    flake6.outlined = out
+    flake6.draw(its, screen)
+
+    flake7 = Flake(8, rad, (pos[0] + rad*2 + 30, pos[1] + rad*2 + 30))
+    flake7.colorful = col
+    flake7.outlined = out
+    flake7.draw(its, screen)
+
+    flake12 = Flake(12, rad, (pos[0] + rad*4 + 60, pos[1] + rad*2 + 30))
+    flake12.colorful = col
+    flake12.outlined = out
+    flake12.draw(its, screen)
+
     pg.display.flip()
 
     running = True
@@ -32,8 +72,6 @@ def main():
                     running = False
             elif e.type == QUIT:
                 running = False
-
-        # pg.display.flip()
 
     pg.quit()
 
